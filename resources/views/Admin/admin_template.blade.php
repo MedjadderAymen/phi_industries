2461e2067da799c399ac9935c12ec2859a3e58f1
Medjadder Aimen <aymen_medjader@hotmail.com>
2020-11-23 20:29:59 +0100

    dakhboard

--- resources/views/Admin/admin_template.blade.php
+++ resources/views/Admin/admin_template.blade.php
@@ -44,7 +44,7 @@
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="{{route('home')}}" class="nav-link">tableau de bord</a>
+                <a href="{{route('home')}}" class="nav-link">Tableau de bord</a>
             </li>
         </ul>
 
