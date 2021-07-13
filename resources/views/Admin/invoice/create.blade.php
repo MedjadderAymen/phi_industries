ca0a7b4e2aa186157854bc1636f39a1ca5decc2c
MedjadderAymen <aymen_medjader@hotmail.com>
2021-07-13 20:50:23 +0100

    init commit

--- resources/views/Admin/invoice/create.blade.php
+++ resources/views/Admin/invoice/create.blade.php
@@ -105,8 +105,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
 
-
-
         ////////////////////////js//////////////////////////////////////
 
         var medications;
